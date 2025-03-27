d3169e17fbfcb0c10ed93f64b89fae81d347827d
dagurf21 <dagur.freyr2820@gmail.com>
2025-03-27 16:54:50 +0000

    Cmake

---
+++ build/CMakeFiles/tetris_remake.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tetris_remake.dir/src/Board.cpp.o"
+  "CMakeFiles/tetris_remake.dir/src/Board.cpp.o.d"
+  "CMakeFiles/tetris_remake.dir/src/Game.cpp.o"
+  "CMakeFiles/tetris_remake.dir/src/Game.cpp.o.d"
+  "CMakeFiles/tetris_remake.dir/src/Tetromino.cpp.o"
+  "CMakeFiles/tetris_remake.dir/src/Tetromino.cpp.o.d"
+  "CMakeFiles/tetris_remake.dir/src/main.cpp.o"
+  "CMakeFiles/tetris_remake.dir/src/main.cpp.o.d"
+  "tetris_remake"
+  "tetris_remake.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tetris_remake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
